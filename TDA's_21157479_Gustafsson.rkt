7aa944dc4680387d94e22bf64c8aecf467571501
Thomas Gustafsson <thomas.gustafsson@usach.cl>
2023-10-03 23:13:49 -0300

    Avance TDA's

--- TDA's_21157479_Gustafsson.rkt
+++ TDA's_21157479_Gustafsson.rkt
@@ -1,4 +1,8 @@
 #lang racket
+(provide (all-defined-out))
+
+
+#|------------------------------------------------------------------------------------------------------|#
 
 ; TDA's minimas:
 ; option, flow, chatbot, system, user, chatHistory.
@@ -8,6 +12,9 @@
 ; system-login, system-logout, system-talk-rec, system-talk-norec, system-synthesis, system-simulate.
 
 #|------------------------------------------------------------------------------------------------------|#
+
+
+
 
 
                      ;-- Implementación TDA option --
@@ -22,13 +29,13 @@ Este TDA representa las opciones/respuestas que puede optar el usuario ante el c
 ;Descripción: Crea opciones a escoger.
 ;Recursividad: ---
 
-(define option(lambda(code message ChatbotCodeLink InitialFlowCodeLink Keyword*)
+(define option(lambda(code message ChatbotCodeLink InitialFlowCodeLink . Keyword)
                 (if (and (integer? code) (>= code 0)
                          (string? message)
                          (integer? ChatbotCodeLink) (>= ChatbotCodeLink 0)
                          (integer? InitialFlowCodeLink) (>= InitialFlowCodeLink 0)
-                         (string? Keyword))
-                    (list code message ChatbotCodeLink InitialFlowCodeLink Keyword*)
+                         (Keyword? Keyword))
+                    (list code message ChatbotCodeLink InitialFlowCodeLink Keyword)
                     option-invalido)))
                 
 #|SELECTORES|#
@@ -50,14 +57,23 @@ Este TDA representa las opciones/respuestas que puede optar el usuario ante el c
 ;Rec: Booleano.
 ;Descripción: Verifica que sea un tipo option y retorna un booleano.
 ;Recurción: Ninguna.                  
-(define option?(lambda(a)
+
+#|(define option?(lambda(a)
                  (and(list? a)
                      (= (lenght a) 5)
-                     (...)
+                     (...)|#
 
-         
+
+(define Keyword?(lambda(L)
+                  (define keyword?(lambda(L1 n)
+                                    (if (
+
+
+                     
+                     
                      ;-- Implementación TDA flow --
 
+#|CONSTRUCTORES:|#                    
 ;Dominio: id(int) x name-msg(String) x Option*(Indica que puede recibir cero o más opciones).
 ;Recorrido: flow.
 ;Descripción: Funcion que construye un flujo de un chatbot y verifica que las opciones creadas no se repitan.
@@ -76,8 +92,17 @@ Este TDA representa las opciones/respuestas que puede optar el usuario ante el c
 #|PERTENENCIA (NO OBLIGATORIO)|#
 
 
-                     ;-- Implementación TDA flow-add-option --
 
+
+
+
+
+
+                     
+
+                     ;-- Implementación TDA flow-add-option --
+                     
+#|CONSTRUCTORES:|#
 ;Dominio: flow x option.
 ;Recorrido: flow.
 ;Descripción: Añade más flujos y verifica que no se repitan.
@@ -96,8 +121,16 @@ Este TDA representa las opciones/respuestas que puede optar el usuario ante el c
 #|PERTENENCIA (NO OBLIGATORIO)|#
 
 
+
+
+
+
+
+                     
+
                      ;-- Implementación TDA chatbot --
 
+#|CONSTRUCTORES:|#                     
 ;Dominio: chatbotID(int) x name(String) x welcomeMessage(String) x startFlowId(int) x  flows*(indicando que puede recibir 0 o más flujos).
 ;Recorrido: chatbot.
 ;Descripción: Construye un chatbot y verifica que no se repita.
@@ -116,9 +149,18 @@ Este TDA representa las opciones/respuestas que puede optar el usuario ante el c
 #|PERTENENCIA (NO OBLIGATORIO)|#
 
 
+
+
+
+
+
+
+                     
+
                      
                         ;-- Implementación TDA chatbot-add-flow --
 
+#|CONSTRUCTORES:|#                     
 ;Dominio: chatbot x flow.
 ;Recorrido: chatbot.
 ;Descripción: Añade un flujo a un chatbot y verifica que no se repita.
@@ -137,8 +179,15 @@ Este TDA representa las opciones/respuestas que puede optar el usuario ante el c
 #|PERTENENCIA (NO OBLIGATORIO)|#
 
 
+
+
+
+
+                     
+
                         ;-- Implementación TDA system --
 
+#|CONSTRUCTORES:|#                     
 ;Dominio: name(string) x InitialChatbotCodeLink(Int) x chatbot*(indicando que pueden recibir 0 o más chatbots).
 ;Recorrido: system.
 ;Descripción: Crea un sistema de chatbots y deja un registro de la fecha de creación.
